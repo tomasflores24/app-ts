252ae55a6356a4433e9552ea302b2cae3200b8d0
Tomas Flores <tomy24ssf@gmail.com>
2022-12-01 10:19:11 -0300

    modificando controller route contries

--- back/src/routes/contries-route.ts
+++ back/src/routes/contries-route.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    console.log("/COUNTRIEs");
+    console.log("/countries, estoyn en el brach pract-contro....");
     return res.json({msg:'Route Create gg COuntries'});
 })
 
