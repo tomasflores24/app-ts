eb337a472a755da6b0d65973649515518e481a19
Tomas Flores <tomy24ssf@gmail.com>
2022-12-02 19:13:52 -0300

    controllers

--- back/src/controllers/contries-controllers.ts
+++ back/src/controllers/contries-controllers.ts
@@ -1,7 +1,7 @@
-// import axios from "axios";
+import axios from "axios";
+import { Request, Response} from 'express'
 
-// export const getCountries = async(_req, res) => {
-//     const countries = await axios('https://restcountries.com/v3/all');
-
-//     return res.status(200).json({ msj: 'aa', data:countries.data });
-// }
+export const getCountries = async(_req:Request, res:Response) => {
+    const countries = await axios('https://restcountries.com/v3/all');
+    return res.json({ msj: 'gggg', data: countries.data });
+}
--- back/src/routes/contries-route.ts
+++ back/src/routes/contries-route.ts
@@ -1,13 +1,14 @@
 import express from 'express'
-// import {getCountries} from '../controllers/contries-controllers'
-import axios from 'axios';
+import {getCountries} from '../controllers/contries-controllers'
+// import axios from 'axios';
 
 const router = express.Router();
 
-// router.get('/', getCountries)
-router.get('/', async(_req, res) => {
-    const countries = await axios('https://restcountries.com/v3/all');   
-    return res.json({data:countries.data})
-})
+router.get('/', getCountries)
+
+// router.get('/', async(_req, res) => {
+    // const countries = await axios('https://restcountries.com/v3/all');   
+    // return res.json({data:countries.data})
+// })
 
 export default router;
